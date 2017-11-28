eb5e80203887cf7e67fd24853585ea82c5003601
olinalvarez <olinalvarez@gmail.com>
2017-11-28 00:49:40 -0800

    fixed pacakge status file

--- PackageStatus.php
+++ PackageStatus.php
@@ -5,7 +5,7 @@
 	<?php
       //this is just according to the database on my laptop.
       $connect = mysqli_connect("localhost", "root", "123456");
-      mysqli_select_db($connect, "packageDB");
+      mysqli_select_db($connect, "packageDB2");
     ?>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/style.css">
@@ -24,7 +24,7 @@
         //trying to get the attribute for the id
         //$package_id = filter_input(INPUT_GET,'link', FILTER_SANITIZE_URL);
         //$packageInfo = mysqli_query($connect, "select pk_PackageName, pk_Description, pk_Status from pk_Package where pk_id = " + $package_id + ";");
-        $packageInfo = mysqli_query($connect, "select pk_PackageName, pk_Description, pk_Status from pk_Package ;");
+        $packageInfo = mysqli_query($connect, "select pk_ID, pk_PackageName, pk_Description, pk_Status from pk_Package ;");
         /*if(is_null($packageInfo)){
           print "<b>";
           print "Package not found!";
@@ -36,6 +36,9 @@
         while ($row = mysqli_fetch_assoc($packageInfo))
         {
           print "<tr>";
+          print "<td>";
+          print $row['pk_ID'];
+          print "</td>";
           print "<td>";
           print $row['pk_PackageName'];
           print "</td>";
