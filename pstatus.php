ad92b7ed9f0a3031f192358c039c706bebf79111
olinalvarez <olinalvarez@gmail.com>
2017-11-27 21:29:40 -0800

    second commit

--- pstatus.php
+++ pstatus.php
@@ -4,11 +4,11 @@
 	<title>Package Status</title>
 	<?php
       //this is just according to the database on my laptop.
-      $connect = mysqli_connect("localhost", "root", "1234");
+      $connect = mysqli_connect("localhost", "root", "123456");
       mysqli_select_db($connect, "packageDB");
     ?>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/style.css">p
   </head>
 </head>
 <body class="packageStatus">
@@ -17,7 +17,7 @@
         <th width="110px">Id</th>
         <th>Name</th>
         <th>Description</th>
-        <th>Status/th>
+        <th>Status</th>
       </tr>
       <?php
         $packageInfo = mysqli_query($connect, "select pk_PackageName, pk_Description, pk_Status from pk_Package");
